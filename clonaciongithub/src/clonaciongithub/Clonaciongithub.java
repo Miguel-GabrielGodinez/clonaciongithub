085541faacbcf1762a61b251b545003513a7e51e
hp <hp@192.168.1.11>
2020-04-29 21:37:15 -0600

    pruebagit

---
+++ clonaciongithub/src/clonaciongithub/Clonaciongithub.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clonaciongithub;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class Clonaciongithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        public class ejemploTablas
+{
+		public static void main(String[] args) {
+		int Ntabla;
+		Scanner teclado = new Scanner(System.in);	
+		System.out.println("TABLAS DE MULTIPLICAR");
+		System.out.println("--------------------------------");
+		System.out.println("Ingrese le número de Tabla:");
+		Ntabla = teclado.nextInt();
+		teclado.nextLine();
+
+		/*INTANCIA DE UNA CLASE = OBJETO*/
+
+		tabla NuevaTabla = new tabla();
+		NuevaTabla.imprimirTabla(Ntabla);
+
+
+	}
+}
+    }
+    
+}
